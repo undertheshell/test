ba36deb5a08386737afb88c942aabd3e7039c25b
undertheshell <zemlyakova99@bk.ru>
2019-04-28 12:42:43 +0300

    Added lab files

---
+++ labs1.js
@@ -0,0 +1,75 @@
+﻿function myFunc1() {
+alert ("задание выполнено");
+}
+
+function  myFunc2() {
+  var str = prompt('"Привет"');
+  var num = 123;
+  var flag = true;
+  var txt = "true";
+  alert("Задание 2:\nstr is " + typeof(str) + "\nnum is " + typeof(num) + "\nflug is " + typeof(flag) + "\ntxt is " + typeof(txt));
+}
+
+function  myFunc3() {
+	var a1 = 5 + 3;
+	var a2 = 5 - 3;
+	var a3 = 5 * 3;
+	var a4 = 5 / 3;
+	var a5;
+	alert("Задание 3:\na1 = " + a1 + "\na2 = " + a2 + "\na3 = " + a3 + "\na4 = "+ a4 + "\na5 = " + a5)
+}
+
+function myFunc4() {
+	var a6 = 5 % 3;
+	var a7 = 3 % 5;
+	var a8 = 5 + '3';
+	var a9 = '5' - 3;
+	var a10 = 75 + 'кг';
+	alert("Задание 4:\na6 = " + a6 + "\na7 = " + a7 + "\na8 = " + a8 + "\na9 = "+ a9 + "\na10 = " + a10)
+}
+
+function myFunc5() {
+	var l = prompt('Введите длину:');
+	var w = prompt('Введите ширину:');
+	alert ("Задание 5: Площадь прямоугольника = " + l*w)
+}
+
+function myFunc6() {
+	var r = prompt('Введите радиус:');
+	var h = prompt('Введите высоту:');
+	alert ("Задание 6: Объем цилиндра = " + (Math.PI*r*r*h))
+}
+
+function myFunc7() {
+	var n = 3;
+	var m = 4;
+	var k = Math.pow(n,2)+Math.pow(m,2);
+	alert ("Задание 7: Длина гипотенузы = " + Math.pow(k, 0.5))
+}
+
+function myFunc8() {
+	var name = prompt('Как Вас зовут?');
+	alert ('Задание 8.1: Вас зовут ' + name + '!');
+}
+function myFunc88() {
+	if (confirm("Нажмите ОК или Отмена")) {
+	    alert("Вы нажали ОК.");
+     }
+	else {
+	    alert("Вы нажали Отмена.");
+     }
+}
+function myFunc888() {
+	var name = prompt("Введите свое имя:")
+	if (confirm("Введеное имя правильное?"))
+	    {
+          alert("Здравствуйте, " + name);
+             }
+}
+
+function myFunc9() {
+	var a = 5, b = 10, c;
+	c = a + b--; 
+	document.write(c);
+}
+
---
+++ labs2.js
@@ -0,0 +1,78 @@
+﻿function function1() {
+var name=prompt("Как Вас зовут?");
+alert(name);
+}
+
+function function2() {
+var number = '3751';
+arr = number.split('');
+arr.sort();
+var result = arr.join('');
+alert(result);
+}
+
+function function3() {
+var num=1;
+num +=12;
+num -=14;
+num *=5;
+num /=7;
+num++;
+num--;
+alert(num);
+}
+
+function function4() {
+var a6 = 5 % 3;
+var a7 = 3 % 5;
+var a8 = 5 + '3';
+var a9 = '5' - 3;
+var a10 = 75 + 'кг';
+alert("a6=" + a6 + "<br>" + "a7=" + a7 + "<br>" + "a8=" + a8 +"<br>"+ "a9=" + a9 +"<br>"+ "a10 = " + a10);
+}
+
+function function5() {
+var arr=['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+alert(arr);
+var day=prompt("введите номер дня недели");
+day = day-1;
+for (var i=0; i<arr.length; i++) {
+if (day==i){
+day=arr[i];
+alert(day);
+break;}}
+
+}
+
+function function6() {
+var a=10, b=3;
+var c=a%b;
+alert(c);
+}
+
+function function7() {
+var d=5, e=10;
+var f= ++d + --e;
+alert(f);
+}
+
+function function8() {
+var aarr = [100, 200, 300, 400];
+var g=0;
+for (var j = 0;  j< aarr.length; j++) {
+g +=aarr[j];}
+alert(g);
+}
+
+function function9() {
+var a=prompt("Введите число");
+if (a==10) {
+alert ("верно");}
+else {
+alert ("неверно");}
+}
+
+function function10() {
+var str = ['Привет,', 'мир', '!'];
+alert(str);
+}
